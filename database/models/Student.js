6eb6b86754a3ca8271726db5148b58703305092a
shadmanrakib <srakibnyc1@gmail.com>
2025-05-11 19:29:42 -0400

    tweak models to adhere to spec

--- database/models/Student.js
+++ database/models/Student.js
@@ -3,34 +3,38 @@
 
 It defines the student model for the database.
 ==================================================*/
-const Sequelize = require('sequelize');  // Import Sequelize
-const db = require('../db');  // Import Sequelize database instance called "db"
+const Sequelize = require("sequelize"); // Import Sequelize
+const db = require("../db"); // Import Sequelize database instance called "db"
 
 const Student = db.define("student", {
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   email: {
     type: Sequelize.STRING,
     allowNull: false,
   },
 
-  url: {
+  imageUrl: {
     type: Sequelize.TEXT,
     allowNull: true,
-    defaultValue: '' 
+    defaultValue: "",
   },
 
   gpa: {
     type: Sequelize.FLOAT,
     allowNull: false,
-  }
+    validate: {
+      min: 0,
+      max: 4,
+    },
+  },
 });
 
 // Export the student model
